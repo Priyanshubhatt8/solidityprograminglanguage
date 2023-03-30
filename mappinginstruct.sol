df4019a2336cf184f4b610d65acdef7903543ce5
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-03-30 10:09:19 +0530

    Add files via upload

---
+++ mappinginstruct.sol
@@ -0,0 +1,18 @@
+//SPDX-License-Identifier:GPL-3.0
+pragma solidity>=0.5.0<0.9.0;
+
+//mapping in structure
+contract demo
+
+{
+    struct Student{
+        string name;
+        uint class;
+    
+    }
+    mapping(uint=>Student)public data;
+    function setter(uint _roll,string memory _name,uint _class)public
+    {
+        data[_roll]=Student(_name,_class); 
+    }
+}
