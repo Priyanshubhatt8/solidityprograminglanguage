815d2419583882153c5eaa0f9bda30342e259ae1
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-03-31 12:05:05 +0530

    Add files via upload

---
+++ bytearray.sol
@@ -0,0 +1,13 @@
+//SPDX-License-Identifier: GPL-3.0
+pragma solidity ^0.8.17;
+
+contract array{
+bytes3 public b3;//3 bytes array
+bytes2 public b2;//2 bytes array
+
+function setter()public {
+    b3='abc';
+    b2='ab';
+
+}
+}
