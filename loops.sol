7091af6c383e3b0a4b1772d2fad6e34dfa503c8e
Priyanshu Bhatt <123248222+Priyanshubhatt8@users.noreply.github.com>
2023-04-10 00:21:13 +0530

    Add files via upload

--- loops.sol
+++ loops.sol
@@ -1,30 +1,25 @@
-//SPDX-License-Identifier: GPL-3.0
-pragma solidity >=0.5.0<0.9.0;
-contract array
-{
-    uint[3] public arr; 
-    uint public count;
-//function loops()public{
-/* while(count<arr.length)
-    {
-        arr[count]=count;
-        count ++;
-    }
-}*/
-/*function forloop()public{
-    for(uint i=0; i<arr.length;i++)
-    {
-        arr[i]=count;
-        count++;
-    }
-}*/
-function dowhileloop() public{
- do{
-    arr[count]=count;
-    count++;
-}while(count<arr.length);
+//SPDX-License-Identifier:GPL-MIT
+pragma solidity ^0.8.0;
+//loops dont work on contract level they work under the function
+contract loops{
+      function loop()public pure returns(uint) {
+        uint count =0;
+
+//         for(uint i=0;i<10;i++){
+// count =count+5;
+//         }
+uint j=0;
+// while(j<10){
+//     count =count+5;
+//     j++;
+// }
+do{
+    count=count+5;
+    j=j+2;
 }
+    while(j<10);
+return count;
+      }
+        
+        
 }
-
-
-
